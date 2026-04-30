0f0293ee93c7890c7c6dcc91024d17e7170baa83
David Ardila <davidardila0607@gmail.com>
2026-04-29 19:48:20 -0500

    cambie el print  linea 118

--- PROYECTO HERRAMIENTAS/calculadora.py
+++ PROYECTO HERRAMIENTAS/calculadora.py
@@ -115,6 +115,7 @@ def _self_test():
         'sqrt(16)': 4.0,
     }
     print('Ejecutando prueb automáticas')
+    print("conflictivo ese print")
     for expr, expected in tests.items():
         try:
             out = safe_eval(expr)
